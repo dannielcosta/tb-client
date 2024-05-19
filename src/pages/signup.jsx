a51dfa8b7aaed8533808cecd387b47908f0d8240
Daniel <dan.costaa.12@gmail.com>
2024-05-19 17:11:45 +0100

    profile testing on phone

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -4,36 +4,36 @@ import { Link } from 'react-router-dom'
 function signup() {
   return (
     <main>
-        <form class="form">
-    <p class="title">Register </p>
-    <p class="message">Signup now and get full access to our app. </p>
-        <div class="flex">
+        <form className="form">
+    <p className="title">Register </p>
+    <p className="message">Signup now and get full access to our app. </p>
+        <div className="flex">
         <label>
-            <input class="inputs" type="text" placeholder="" required=""/>
+            <input className="inputs" type="text" placeholder="" required=""/>
             <span>Firstname</span>
         </label>
 
         <label>
-            <input class="inputs" type="text" placeholder="" required=""/>
+            <input className="inputs" type="text" placeholder="" required=""/>
             <span>Lastname</span>
         </label>
     </div>  
             
     <label>
-        <input class="inputs" type="email" placeholder="" required=""/>
+        <input className="inputs" type="email" placeholder="" required=""/>
         <span>Email</span>
     </label> 
         
     <label>
-        <input class="inputs" type="password" placeholder="" required=""/>
+        <input className="inputs" type="password" placeholder="" required=""/>
         <span>Password</span>
     </label>
     <label>
-        <input class="inputs" type="password" placeholder="" required=""/>
+        <input className="inputs" type="password" placeholder="" required=""/>
         <span>Confirm password</span>
     </label>
-    <button class="submit">Submit</button>
-    <p class="signin">Already have an acount ? <Link to="/"><a href="#">Login</a></Link> </p>
+    <button className="submit">Submit</button>
+    <p className="signin">Already have an acount ? <Link to="/"><a href="#">Login</a></Link> </p>
 </form>
     </main>
   )
